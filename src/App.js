8ab2246cdcb8a0802e10b4fbdca3fcedabbbe2fe
raymondtju <raymond.rtju@gmail.com>
2023-01-01 22:52:38 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Categories from "./pages/Categories";
+import CategoryEdit from "./pages/Categories/edit";
+import CategoryCreate from "./pages/Categories/create";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/categories/create" element={<CategoryCreate />} />
+        <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
