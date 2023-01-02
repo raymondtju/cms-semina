8373f9f2ac8aa60437a4bc642f8a6c9248552115
raymondtju <raymond.rtju@gmail.com>
2023-01-02 21:21:00 +0700

    add get and post data function

---
+++ src/utils/fetch.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { config } from "../configs";
+
+export async function getData(url, params) {
+  try {
+    const { token } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+
+    return await axios.get(`${config.url_dev}${url}`, {
+      params,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function postData(url, payload, formData) {
+  try {
+    const { token } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+
+    return await axios.post(`${config.url_dev}${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": formData ? "multipart/form-data" : "application/json",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+}
