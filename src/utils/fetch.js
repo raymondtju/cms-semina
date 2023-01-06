b593a18ccd56c37fb76a7d130e835408778c6ca7
raymondtju <raymond.rtju@gmail.com>
2023-01-06 22:56:37 +0700

    add update data api

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { config } from "../configs";
+// import handleError from "./handleError";
 
 export async function getData(url, params) {
   try {
@@ -28,6 +29,38 @@ export async function postData(url, payload, formData) {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": formData ? "multipart/form-data" : "application/json",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function updateData(url, payload) {
+  try {
+    const { token } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+
+    return await axios.put(`${config.url_dev}${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function deleteData(url) {
+  try {
+    const { token } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+
+    return await axios.delete(`${config.url_dev}${url}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
       },
     });
   } catch (err) {
