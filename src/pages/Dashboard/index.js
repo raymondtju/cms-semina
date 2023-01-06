ecef239bde828851957ad0d5d2b1bf7010966fa6
raymondtju <raymond.rtju@gmail.com>
2023-01-06 22:59:46 +0700

    dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import SBreadcrumb from "../../components/Breadcrumb";
-import SNavbar from "../../components/Navbar";
 
 export default function Dashboard() {
-  if (!localStorage.getItem("token")) {
-    return <Navigate to="/signin" replace={true} />;
-  }
   return (
-    <>
-      <SNavbar />
-
-      <Container>
-        <SBreadcrumb />
-      </Container>
-    </>
+    <Container>
+      <SBreadcrumb />
+      <h1>Dashboard</h1>
+    </Container>
   );
 }
