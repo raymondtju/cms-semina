a410fb974b1bafd962e9cfa56654ef198ca57fc9
raymondtju <raymond.rtju@gmail.com>
2023-01-06 22:57:07 +0700

    add navbar include access

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import NavLink from "../NavLink";
+import NavLink from "../NavAccess";
+
+import {
+  accessCategories,
+  accessTalents,
+  accessEvents,
+  accessPayments,
+  accessParticipant,
+  accessOrders,
+} from "../../const/access";
 
 function SNavbar() {
   const navigate = useNavigate();
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    const fetch = () => {
+      let { role } = localStorage.getItem("auth")
+        ? JSON.parse(localStorage.getItem("auth"))
+        : {};
+      setRole(role);
+    };
+    fetch();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.href = "/signin";
+  };
+
   return (
     <Navbar bg="light" variant="light">
       <Container>
         <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
         <Nav className="me-auto">
-          <NavLink action={() => navigate("/")}>Home</NavLink>
-          <NavLink action={() => navigate("/categories")}>Categories</NavLink>
-          <NavLink action={() => navigate("/speakers")}>Speakers</NavLink>
-          <NavLink action={() => navigate("/events")}>Events</NavLink>
-          <NavLink action={() => navigate("/participant")}>Participant</NavLink>
-          <NavLink action={() => navigate("/Transactions")}>
-            Transactions
+          <NavLink
+            role={role}
+            roles={accessCategories.view}
+            action={() => navigate("/")}
+          >
+            Home
           </NavLink>
+          <NavLink
+            role={role}
+            roles={accessCategories.view}
+            action={() => navigate("/categories")}
+          >
+            Categories
+          </NavLink>
+          <NavLink
+            role={role}
+            roles={accessTalents.view}
+            action={() => navigate("/talents")}
+          >
+            Talents
+          </NavLink>
+          <NavLink
+            role={role}
+            roles={accessPayments.view}
+            action={() => navigate("/payments")}
+          >
+            Payment
+          </NavLink>
+          {/* <NavLink
+            role={role}
+            roles={organizers.view}
+            action={() => navigate('/organizers')}
+          >
+            Oranizer
+          </NavLink> */}
+          <NavLink
+            role={role}
+            roles={accessEvents.view}
+            action={() => navigate("/events")}
+          >
+            Events
+          </NavLink>
+          <NavLink
+            role={role}
+            roles={accessParticipant.view}
+            action={() => navigate("/participant")}
+          >
+            Participant
+          </NavLink>
+          <NavLink
+            role={role}
+            roles={accessOrders.view}
+            action={() => navigate("/orders")}
+          >
+            Orders
+          </NavLink>
+        </Nav>
+        <Nav className="justify-content-end">
+          <Nav.Link onClick={() => handleLogout()}>Logout</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
