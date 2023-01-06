22d7435df0e37d8ceed4092c06e0372484045d0c
raymondtju <raymond.rtju@gmail.com>
2023-01-06 22:57:27 +0700

    access control

---
+++ src/const/access.js
@@ -0,0 +1,40 @@
+export const accessCategories = {
+  view: ["organizer"],
+  create: ["organizer"],
+  edit: ["organizer"],
+  delete: ["organizer"],
+};
+
+export const accessTalents = {
+  view: ["organizer"],
+  create: ["organizer"],
+  edit: ["organizer"],
+  delete: ["organizer"],
+};
+
+export const accessEvents = {
+  view: ["organizer"],
+  create: ["organizer"],
+  edit: ["organizer"],
+  delete: ["organizer"],
+};
+
+export const accessParticipant = {
+  view: ["organizer"],
+  create: ["organizer"],
+  edit: ["organizer"],
+  delete: ["organizer"],
+};
+export const accessPayments = {
+  view: ["organizer"],
+  create: ["organizer"],
+  edit: ["organizer"],
+  delete: ["organizer"],
+};
+
+export const accessOrders = {
+  view: ["organizer", "admin", "owner"],
+  create: ["organizer", "admin", "owner"],
+  edit: ["organizer", "admin", "owner"],
+  delete: ["organizer", "admin", "owner"],
+};
