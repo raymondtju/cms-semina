c4ac8c71b29b4df551877e59abbe855635093775
raymondtju <raymond.rtju@gmail.com>
2023-01-06 22:58:47 +0700

    app route, app styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import Signin from "./pages/Signin";
-import Dashboard from "./pages/Dashboard";
-import Categories from "./pages/Categories";
-import CategoryEdit from "./pages/Categories/edit";
-import CategoryCreate from "./pages/Categories/create";
+import "./App.css";
 import { listen } from "./redux/listener";
+import { AppRoutes } from "./routes";
 
 function App() {
   useEffect(() => {
@@ -15,13 +12,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/categories/create" element={<CategoryCreate />} />
-        <Route path="/categories/edit/:id" element={<CategoryEdit />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
