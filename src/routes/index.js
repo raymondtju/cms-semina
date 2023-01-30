876f68ff9912bf8ea02a3cd005def3fc7d161f55
raymondtju <raymond.rtju@gmail.com>
2023-01-30 13:10:10 +0700

    add talents and payments path

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ import GuestRoute from "../components/GuestRoute";
 
 import Signin from "../pages/Signin";
 import { HomeRoute } from "./HomeRoute";
-// import { TalentsRoute } from "./TalentsRoute";
-// import { PaymentsRoute } from "./PaymentsRoute";
+import { TalentsRoute } from "./TalentsRoute";
+import { PaymentsRoute } from "./PaymentsRoute";
 import SNavbar from "../components/Navbar";
 import { CategoriesRoute } from "./CategoriesRoute";
 // import { EventsRoute } from "./EventsRoute";
@@ -33,9 +33,9 @@ export function AppRoutes() {
       >
         <Route path="dashboard/*" element={<HomeRoute />} />
         <Route path="categories/*" element={<CategoriesRoute />} />
-        {/* <Route path="talents/*" element={<TalentsRoute />} />
+        <Route path="talents/*" element={<TalentsRoute />} />
         <Route path="payments/*" element={<PaymentsRoute />} />
-        <Route path="events/*" element={<EventsRoute />} />
+        {/* <Route path="events/*" element={<EventsRoute />} />
         <Route path="orders/*" element={<OrdersRoute />} /> */}
         <Route path="" element={<Navigate to="/dashboard" replace={true} />} />
       </Route>
